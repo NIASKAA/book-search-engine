500de85a3990cc2e4782ca9978577a66d8c4b4b7
NIASKA <mkman751@yahoo.com>
2021-07-21 15:08:22 -0500

    added typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,28 @@
+const {ggl} = require('apollo-server-express')
+
+const typeDefs = gql`
+    type Book {
+        authors: String
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+    
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
+    
+    type Query {
+        me: User
+    }
+    
+    type Mutation {
+        login(email: String!, password: String!): Auth 
+        addUser(username: String!, email: String!, password: String!): Auth
+    }`
